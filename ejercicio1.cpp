8b269fef2af837a75811b7975c9cf60f67197664
stipcorimaya <scorimayac@unjbg.edu.pe>
2024-07-13 02:15:23 -0500

    añadiendo los datos principales

---
+++ ejercicio1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int matrizA[3][3]= {
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+    };
+	int matrizB[3][3]={
+    {9,8,7},
+    {6,5,4},
+    {3,2,1}
+    };
+    int acumulador;
+	int matrizC[3][3];
+	}
+return 0;
+}
