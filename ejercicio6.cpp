6c9976a0d576ee4a0bf26c06494efd16059e6e27
stipcorimaya <scorimayac@unjbg.edu.pe>
2024-07-17 01:48:19 -0500

    finalizando ejercicio 6: reflejo

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -12,6 +12,21 @@ int main(){
 	};
 	cout<<"el reflejo de la matriz: "<<endl;
 	int matrizA[5][5];
-	
+	for(int i=0;i<5;i++){
+		cout<<"| ";
+		for(int m=0;m<5;m++){
+			cout<<matriz[i][m]<<" ";
+		}
+		if(i==2){
+			cout<<" |  =  | ";
+		}else{
+		cout<<" |     | ";			
+		}
+		for(int j=0;j<5;j++){
+			matrizA[i][j]=matriz[i][4-j];
+			cout<<matrizA[i][j]<<" ";
+		}
+		cout<<" |"<<endl;
+	}
 	return 0;
 }
