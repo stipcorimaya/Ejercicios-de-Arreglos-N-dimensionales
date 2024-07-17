b034e55adfed1bad9de05a0cdd4161c476ca92e8
stipcorimaya <scorimayac@unjbg.edu.pe>
2024-07-17 13:15:06 -0500

    temirnando la eespiral ejercicio 8

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -10,6 +10,28 @@ int main(){
 	{16,17,18,19,20},
 	{21,22,23,24,25}
 	};
-	
+	for(int j=0;j<2;j++){
+		for(int k=j;k<5-j;k++){
+			if(k==j){
+				for(int m=j;m<5-j;m++){
+				    cout<<matriz[0+j][m]<<" ";
+				}
+			}else{
+		        cout<<matriz[k][4-j]<<" ";
+			}
+		}
+		for(int o=4-j;o>0+j;o--){
+			if(o==4-j){
+				for(int n=3-j;n>=0+j;n--){
+				cout<<matriz[4-j][n]<<" ";
+				}
+			}else{
+				cout<<matriz[o][j]<<" ";
+			}
+		}
+		if(j==1){
+			cout<<matriz[2][j+1];
+		}
+	}
 	return 0;
 }
