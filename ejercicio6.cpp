fb473663d60314fc95d9bfab59f3efbe351a7cca
stipcorimaya <scorimayac@unjbg.edu.pe>
2024-07-17 01:39:48 -0500

    iniciando los datos ejercicio 6

---
+++ ejercicio6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int matriz[5][5]={
+		{1,2,3,4,5},
+		{6,7,8,9,1},
+		{2,3,4,5,6},
+		{7,8,9,1,2},
+		{3,4,5,6,7}
+	};
+	cout<<"el reflejo de la matriz: "<<endl;
+	int matrizA[5][5];
+	
+	return 0;
+}
