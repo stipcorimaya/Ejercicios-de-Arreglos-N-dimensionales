4215966cb06ce97061895ae55cabae58c79c54fc
stipcorimaya <scorimayac@unjbg.edu.pe>
2024-07-17 01:06:10 -0500

    iniciando los datos ejercicio 4

---
+++ ejercicio4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int matriz[4][4]={
+		{1,2,3,4},
+		{5,6,7,8},
+		{9,10,11,12},
+		{13,14,15,16}
+	};
+	int matrizA[4][4];
+	return 0;
+}
