556b5a2e0a6a76ede1c1694c5dcd138b7e5247f9
stipcorimaya <scorimayac@unjbg.edu.pe>
2024-07-17 00:42:28 -0500

    terminando ejercicio 2

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -10,6 +10,27 @@ int main(){
 	};
 	int buscador;
 	int contador;
-	
+	cout<<"segun a la matriz: "<<endl;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cout<<matriz[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<"escribir el valor que se debe encontrar: ";
+	cin>>buscador;
+	cout<<"la ubicaion del valor de "<<buscador<<endl;
+	for(int m=0;m<3;m++){
+		for(int n=0;n<3;n++){
+			if(matriz[m][n]==buscador){
+				cout<<"-es la Fila "<<m<<" ,Columna "<<n<<endl;
+			}else{
+				contador++;
+			}
+		}
+	}
+	if(contador==9){
+		cout<<"no se encuentra en la matriz";
+	}
 	return 0;
 }
