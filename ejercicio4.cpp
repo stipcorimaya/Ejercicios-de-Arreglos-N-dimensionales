266222704beea3b5a13ca10292bbb66ffc950123
stipcorimaya <scorimayac@unjbg.edu.pe>
2024-07-17 01:14:31 -0500

    finalizando el ejercicio 4

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -10,5 +10,22 @@ int main(){
 		{13,14,15,16}
 	};
 	int matrizA[4][4];
+	int a,b;
+	cout<<"escibe un numero"<<endl;
+	cin>>a;
+	cout<<"escribe la fila que quieras intercambiar"<<endl;
+	cin>>b;
+	for(int i=0;i<4;i++){
+		for(int j=0;j<4;j++){
+			if(i==a or i==b){
+				matrizA[a][j]=matriz[b][j];
+				matrizA[b][j]=matriz[a][j];
+			}else{
+			matrizA[i][j]=matriz[i][j];		
+			}
+			cout<<matrizA[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 	return 0;
 }
