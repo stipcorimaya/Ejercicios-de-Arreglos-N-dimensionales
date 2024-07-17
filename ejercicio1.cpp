b0f6411eb6ab3c856c1dff93662a864263f60185
stipcorimaya <scorimayac@unjbg.edu.pe>
2024-07-17 00:17:04 -0500

    Completando ejercicio 1

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -15,6 +15,34 @@ int main(){
     };
     int acumulador;
 	int matrizC[3][3];
+		
+	for(int i=0;i<3;i++){
+		cout<<"|";		
+		for(int n=0;n<3;n++){
+			cout<<matrizA[i][n]<<" ";
+		}
+		if(i==1){
+		cout<<"|  x  |";	
+		}else{
+		cout<<"|     |";			
+		}
+		for(int o=0;o<3;o++){
+		cout<<matrizB[i][o]<<" ";
+	    }
+		if(i==1){
+		cout<<"|  =  |";	
+		}else{
+		cout<<"|     |";			
+		}
+		for(int k=0; k<3; k++){
+			for(int j=0; j<3;j++){
+			acumulador=(matrizA[i][j]*matrizB[j][k])+acumulador;
+			}
+			matrizC[i][k]=acumulador;
+			acumulador=0;			
+			cout<<matrizC[i][k]<<" ";
+		}
+		cout<<"|"<<endl;
 	}
-return 0;
+	return 0;
 }
