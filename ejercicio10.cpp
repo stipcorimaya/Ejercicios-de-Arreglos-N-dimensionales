4d82b6d0a8a7997c26ee7cd055be60b783c336ef
stipcorimaya <scorimayac@unjbg.edu.pe>
2024-07-17 01:57:34 -0500

    iniciando datos ejercicio 10

---
+++ ejercicio10.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int main(){
+	int matriz[10][10];
+	int ubicacion[10][10];
+	srand(time(0));
+	int minas;
+	
+	return 0;
+}
